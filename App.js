e348df170f24023ace5cbbf18b328e008ceacb21
Gustavo Marques <142382220+Gustavo-Marques-Santiago-Sanches@users.noreply.github.com>
2024-05-13 22:04:04 -0300

    5.0
    Produtos

--- App.js
+++ App.js
@@ -1,79 +1,34 @@
-import {useFonts, SpaceGrotesk_300Light, SpaceGrotesk_700Bold} from '@expo-google-fonts/space-grotesk';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, View} from 'react-native';
 
-import { View } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import Produto from './src/Telas/Produtos/Index';
-import Sobre from './src/Telas/Sobre/Sobre_Nos';
-import mockProd from './src/mocks/Produto';
-import mockSobre from './src/mocks/Sobre';
-
-function MenuKit(){
-  return <View>
-          <Produto {...mockProd}/>
-        </View>
-}
-
-function SobreN(){
-  return <View>
-          <Sobre {...mockSobre}/>
-        </View>
-}
-
-const Tab = createBottomTabNavigator();
-
-function TabsMenu(){
-  return <Tab.Navigator 
-          screenOptions={ ({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              let iconName;
-              
-              if(route.name === "Sobre nós"){
-                iconName = focused
-                ? 'help-circle'
-                : 'help-circle-outline';
-              } else if(route.name === "Kit"){
-                iconName = focused
-                ? 'basket'
-                : 'basket-outline';
-              } else if(route.name === "Produtos"){
-                iconName = focused
-                ? 'list'
-                : 'list-outline';
-              } else if(route.name === "Lista de Desejos"){
-                iconName = focused
-                ? 'heart'
-                : 'heart-outline';
-              }
-
-              return <Ionicons name={iconName} size={size} color={color}/>
-            },
-            tabBarActiveTintColor: 'black',
-            tabBarInactiveTintColor: 'gray',
-            tabBarHideOnKeyboard: true,
-          })}>
-            <Tab.Screen name="Sobre nós" component={SobreN}/>
-            <Tab.Screen name="Kit" component={MenuKit}/>
-            <Tab.Screen name="Produtos" component={MenuKit}/>
-            <Tab.Screen name="Lista de Desejos" component={MenuKit}/>
-          </Tab.Navigator>
-}
+//Importa a imagem
+import miniP from './assets/miniP.png';
 
 export default function App() {
-  //Carrega a fonte para o meu projeto
-  const [fonteCarregada] = useFonts({
-    "SpaceGRegular" : SpaceGrotesk_300Light,
-    "SpaceGBold" : SpaceGrotesk_700Bold,
-  })
-
-  //Checa se as fontes já foram carregadas
-  if(!fonteCarregada){
-    return <View />
-  }
-
-  return <NavigationContainer>
-          <TabsMenu />
-        </NavigationContainer>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hello World!</Text>
+      <Text>Este é meu primeiro app!</Text>
+      <Image style={styles.imagem} source={miniP}/>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+  imagem: {
+    width: '70%',
+    height: '40%',
+  },
+});
