0712edf506d3405ca11ca0f4c59c197b674507e6
Gustavo Marques <142382220+Gustavo-Marques-Santiago-Sanches@users.noreply.github.com>
2024-08-14 22:08:37 -0300

    Video

--- src/Telas/Sobre/Sobre_Nos.js
+++ src/Telas/Sobre/Sobre_Nos.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Image} from 'react-native';
+import {Video, ResizeMode} from 'expo-av';
 
 import Style from './Bonito/Detalhamento'; //CSS
 import Texto from '../../componentes/Texto'; //Componente de exibição de texto
 
-export default function Sobre({tela}){
+import Video_ursinhos from '../../../assets/ursinhos_video.mp4';
 
-    return <ScrollView style={Style.fundo}>
+
+
+export default function Sobre({tela}){
+  
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  
+  return <ScrollView style={Style.fundo}>
     <Image style={Style.logo} source={tela.logo} resizeMode='contain'/>
     <Texto style={Style.desc}>{tela.descricao_pri}</Texto>
     <Texto style={Style.titulo}>{tela.obj}</Texto>
     <Texto style={Style.desc}>{tela.descricao_sec}</Texto>
     <Image style={Style.imagem} source={tela.img}/>
+    <Video ref={video} 
+      style={{width:320, height: 200}}
+      source={Video_ursinhos}
+      useNativeControls
+      resizeMode={ResizeMode.CONTAIN}
+      isLooping
+      onPlaybackStatusUpdate={status => setStatus(() => status)}/>
   </ScrollView>
 }
