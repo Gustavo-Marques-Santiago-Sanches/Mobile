15aa59f597cdda31dff54d548819026a78487f62
Gustavo Marques <142382220+Gustavo-Marques-Santiago-Sanches@users.noreply.github.com>
2024-10-30 21:43:05 -0300

    API funcionando corretamente

--- src/Telas/ListaDesejos/lista.js
+++ src/Telas/ListaDesejos/lista.js
@@ -3,6 +3,7 @@ import { View, FlatList, Alert } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import axios from 'axios';
+import { Card } from "react-native-paper";
 
 import Texto from '../../componentes/Texto';
 import ListaItem from "./listaItem";
@@ -12,6 +13,8 @@ import Botao from "../Produtos/Componentes/Botao";
 export default function Lista(){
 
     const [listData, setListData] = useState([]);
+    const [apiImagem, setapiImagem] = useState();
+    const [apiNome, setapiNome] = useState();
 
     //Função para capturar os dados do AsyncStorage
     const loadListData = async () => {
@@ -37,24 +40,45 @@ export default function Lista(){
         navigation.reset({index: 0, routes: [{name: 'Lista de Desejos'}]});
     }
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
-    async function api(){
-        const num = getRandomInt(100);
+    async function api() {
+        const num = getRandomInt(10, 50);
         const numTexto = JSON.stringify(num);
         console.log(numTexto);
-        
-        axios.get('https://api.escuelajs.co/api/v1/products/' + numTexto).then(function (response) {
-            // aqui acessamos o corpo da resposta:
-            console.log(response.data);
-        })
+    
+            const response = await axios.get('https://api.escuelajs.co/api/v1/products/' + numTexto);
+            
+            // Acessamos o corpo da resposta:
+            const data = response.data;
+            console.log(data);
+            
+            // Acessando informações específicas
+            setapiNome(data.title);
+            setapiImagem(data.images[0]);
+            
+            // console.log(nome, imagem);
     }
 
     return <View style={styles.listaContainer}>
             <Texto style={styles.titulo}>Lista de Desejos</Texto>
+            <Botao textoBotao={"Sugestões de Item"} clickBotao={()=> api()}></Botao>
+            {apiImagem &&
+                <Card mode='contained' style={styles.card}>
+                    <Card.Content>
+                        <Card.Cover source={{uri : apiImagem}} style={styles.imagem}/>
+                        <Texto style={styles.nomeProduto}>{apiNome}</Texto>
+                    </Card.Content>
+                </Card>
+            }
+
             <Texto style={styles.textoLista}>Estes são os produtos adicionados na sua Lista de Desejos</Texto>
+
+            
 
             <FlatList
                 data={listData}
@@ -63,6 +87,5 @@ export default function Lista(){
                 numColumns={2}
             />
             <Botao textoBotao={"Apagar Lista de Desejos"} clickBotao={()=> clearAsyncStorage()}></Botao>
-            <Botao textoBotao={"API"} clickBotao={()=> api()}></Botao>
     </View>
 }
